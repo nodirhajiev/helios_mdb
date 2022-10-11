45824d82e4c0a9346c8237ae6b4a165989cc154b
NXD <38133441+nodirhajiev@users.noreply.github.com>
2022-10-11 16:44:52 +0500

    Update update.py

--- update.py
+++ update.py
@@ -48,8 +48,8 @@ if UPSTREAM_REPO is not None:
         srun(["rm", "-rf", ".git"])
 
     update = srun([f"git init -q \
-                     && git config --global user.email arshtwitterbot@gmail.com \
-                     && git config --global user.name helios \
+                     && git config --global user.email nodir.hajiev@gmail.com \
+                     && git config --global user.name nodirhajiev \
                      && git add . \
                      && git commit -sm update -q \
                      && git remote add origin {UPSTREAM_REPO} \
